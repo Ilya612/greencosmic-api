5cb90d095152b7d7dcebcb041ecb9ecf9b28de7a
Ilya612 <ilyapronin81@gmail.com>
2022-03-17 11:57:43 +0300

    asd

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -33,24 +33,44 @@ class paymentController {
       const event = req.body;
       console.log(event);
       switch (event.type) {
-        case "payment_intent.succeeded":
-          const paymentIntent = event.data.object;
-          // Then define and call a method to handle the successful payment intent.
-          // handlePaymentIntentSucceeded(paymentIntent);
+        case "payment_intent.amount_capturable_updated":
+          const amount_capturable_updated = event.data.object;
+          // Then define and call a function to handle the event payment_intent.amount_capturable_updated
           break;
-        case "payment_method.attached":
-          const paymentMethod = event.data.object;
-          // Then define and call a method to handle the successful attachment of a PaymentMethod.
-          // handlePaymentMethodAttached(paymentMethod);
+        case "payment_intent.canceled":
+          const canceled = event.data.object;
+          // Then define and call a function to handle the event payment_intent.canceled
+          break;
+        case "payment_intent.created":
+          const created = event.data.object;
+          // Then define and call a function to handle the event payment_intent.created
+          break;
+        case "payment_intent.payment_failed":
+          const payment_failed = event.data.object;
+          // Then define and call a function to handle the event payment_intent.payment_failed
+          break;
+        case "payment_intent.processing":
+          const processing = event.data.object;
+          // Then define and call a function to handle the event payment_intent.processing
+          break;
+        case "payment_intent.requires_action":
+          const requires_action = event.data.object;
+          // Then define and call a function to handle the event payment_intent.requires_action
+          break;
+        case "payment_intent.succeeded":
+          console.log("pososi moy hui");
+          const succeeded = event.data.object;
+          // Then define and call a function to handle the event payment_intent.succeeded
           break;
         // ... handle other event types
         default:
           console.log(`Unhandled event type ${event.type}`);
       }
-      return res.status(200);
+      console.log("Eran Eyger");
+      return res.status(200).json({ received: true });
     } catch (error) {
       console.log(error);
-      res.status(400).json(error);
+      return res.status(400).json(error);
     }
   }
 }

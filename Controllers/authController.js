4b0c58b1e65b7f0abe23339e919300b14f588833
Ilya612 <ilyapronin81@gmail.com>
2022-03-17 22:53:30 +0300

    asfff

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -33,7 +33,7 @@ class controller {
   }
   async login(req, res, next) {
     try {
-      console.log(req);
+      console.log(req.headers);
       const candidate = await authService.login(req.body);
 
       res.cookie("refreshToken", candidate.refreshToken, {

cf48aabbf95e2f1b74d69fd0099cef2dfaefbd34
Ilya612 <ilyapronin81@gmail.com>
2022-03-17 19:23:49 +0300

    asd

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://greencosmic-study.vercel.app/"
+    "https://greencosmic-study.vercel.app"
   );
 
   // Request methods you wish to allow

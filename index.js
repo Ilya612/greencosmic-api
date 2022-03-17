4928d6930c947f3c5f2eeb4cacb9ea3c0a4b14f6
Ilya612 <ilyapronin81@gmail.com>
2022-03-17 21:17:09 +0300

    Asfdffg

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ app.use(function (req, res, next) {
   // Pass to next layer of middleware
   next();
 });*/
-app.use(cookieParser());
-app.use(express.json());
+
 app.use(
   cors({
     origin: "https://greencosmic-study-iviwj8vch-ilya612.vercel.app",
@@ -53,6 +52,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use(cookieParser());
+app.use(express.json());
 app.use("/api", router);
 app.use(errorMiddleware);
 

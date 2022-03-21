298a437adb6d350106f95acab5701ff60f5e0774
Ilya612 <ilyapronin81@gmail.com>
2022-03-21 15:57:24 +0300

    ASdsadasd

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -100,7 +100,7 @@ class controller {
       if (!req.user) {
         return res.status(400).json({ message: "error" });
       }
-
+      console.log(req.user);
       return res.status(200).json(req.user);
     } catch (error) {
       console.log(error);

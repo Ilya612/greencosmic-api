db3407197d49db917e9a352ffde5d8e0281dd39b
Ilya612 <ilyapronin81@gmail.com>
2022-03-22 13:14:26 +0300

    asdasd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://greencosmic-study-b9fxp92p1-ilya612.vercel.app"
+    "https://greencosmic-study-olqucbwnv-ilya612.vercel.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
